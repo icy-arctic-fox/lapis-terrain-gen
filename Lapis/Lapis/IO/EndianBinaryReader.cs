53e41941d3aedcefff8a0bc56a3d26d4a8d23601
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-03 23:12:50 +0000

    Adding a binary reader and writer that can handle big and little endian. Added insertion and extraction for base value types in byte arrays.

---
+++ Lapis/Lapis/IO/EndianBinaryReader.cs
@@ -0,0 +1,206 @@
+﻿using System.IO;
+using Lapis.Utility;
+
+namespace Lapis.IO
+{
+	/// <summary>
+	/// A stream reader that can read in little and big endian
+	/// </summary>
+	public class EndianBinaryReader : BinaryReader
+	{
+		private readonly Endian _endian;
+		private readonly byte[] _buffer = new byte[sizeof(double)];
+
+		// A buffer is kept internally so a new byte array doesn't need to be made for every read call.
+		// This doubles as an internal lock, since two reads going on at the same time would be bad.
+
+		/// <summary>
+		/// Endian being used for reading
+		/// </summary>
+		public Endian Endian
+		{
+			get { return _endian; }
+		}
+
+		/// <summary>
+		/// Creates a new endian-aware binary reader
+		/// </summary>
+		/// <param name="input">Input stream to read from</param>
+		/// <param name="e">Endian to use</param>
+		public EndianBinaryReader (Stream input, Endian e)
+			: base(input)
+		{
+			_endian = e;
+		}
+
+		/// <summary>
+		/// Creates a new endian-aware binary reader
+		/// </summary>
+		/// <param name="input">Input stream to read from</param>
+		/// <param name="encoding">Text encoding</param>
+		/// <param name="e">Endian to use</param>
+		public EndianBinaryReader (Stream input, System.Text.Encoding encoding, Endian e = Endian.Big)
+			: base(input, encoding)
+		{
+			_endian = e;
+		}
+
+		/// <summary>
+		/// Reads a double-precision floating point number and advances the stream by 8 bytes
+		/// </summary>
+		/// <returns>A double</returns>
+		public override double ReadDouble ()
+		{
+			double value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(double));
+				value = _buffer.ToDouble(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed short integer and advances the stream by 2 bytes
+		/// </summary>
+		/// <returns>A signed short</returns>
+		public override short ReadInt16 ()
+		{
+			short value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(short));
+				value = _buffer.ToShort(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed integer and advances the stream by 4 bytes
+		/// </summary>
+		/// <returns>A signed integer</returns>
+		public override int ReadInt32 ()
+		{
+			int value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(int));
+				value = _buffer.ToInt(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed long integer and advances the stream by 8 bytes
+		/// </summary>
+		/// <returns>A signed long</returns>
+		public override long ReadInt64 ()
+		{
+			long value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(long));
+				value = _buffer.ToLong(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a single-precision floating point number and advances the stream by 4 bytes
+		/// </summary>
+		/// <returns>A float</returns>
+		public override float ReadSingle ()
+		{
+			float value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(float));
+				value = _buffer.ToFloat(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned short integer and advances the stream by 2 bytes
+		/// </summary>
+		/// <returns>An unsigned short</returns>
+		public override ushort ReadUInt16 ()
+		{
+			ushort value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(ushort));
+				value = _buffer.ToUShort(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned integer and advances the stream by 4 bytes
+		/// </summary>
+		/// <returns>An unsigned integer</returns>
+		public override uint ReadUInt32 ()
+		{
+			uint value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(uint));
+				value = _buffer.ToUInt(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned long integer and advances the stream by 8 bytes
+		/// </summary>
+		/// <returns>An unsigned long</returns>
+		public override ulong ReadUInt64 ()
+		{
+			ulong value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, sizeof(ulong));
+				value = _buffer.ToULong(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads a signed integer and advances the stream by 3 bytes
+		/// </summary>
+		/// <returns>A signed integer</returns>
+		public int ReadInt24 ()
+		{
+			int value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, 3);
+				if(Endian.Big == _endian)
+					_buffer[0] = 0;
+				else
+					_buffer[3] = 0;
+				value = _buffer.ToInt(0, _endian);
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// Reads an unsigned integer and advances the stream by 3 bytes
+		/// </summary>
+		/// <returns>An unsigned integer</returns>
+		public uint ReadUInt24 ()
+		{
+			uint value;
+			lock(_buffer)
+			{
+				base.Read(_buffer, 0, 3);
+				if(Endian.Big == _endian)
+					_buffer[0] = 0;
+				else
+					_buffer[3] = 0;
+				value = _buffer.ToUInt(0, _endian);
+			}
+			return value;
+		}
+	}
+}
---
+++ Lapis/Lapis/IO/EndianBinaryWriter.cs
@@ -0,0 +1,170 @@
+﻿using System.IO;
+using Lapis.Utility;
+
+namespace Lapis.IO
+{
+	/// <summary>
+	/// A stream writer that can read in little and big endian
+	/// </summary>
+	public class EndianBinaryWriter : BinaryWriter
+	{
+		private readonly Endian _endian;
+		private readonly byte[] _buffer = new byte[sizeof(double)];
+
+		// A buffer is kept internally so a new byte array doesn't need to be made for every write call.
+		// This doubles as an internal lock, since two writes going on at the same time would be bad.
+
+		/// <summary>
+		/// Endian being used for writing
+		/// </summary>
+		public Endian Endian
+		{
+			get { return _endian; }
+		}
+
+		/// <summary>
+		/// Creates a new endian-aware binary writer
+		/// </summary>
+		/// <param name="output">Output stream to write to</param>
+		/// <param name="e">Endian to use</param>
+		public EndianBinaryWriter (Stream output, Endian e)
+			: base(output)
+		{
+			_endian = e;
+		}
+
+		/// <summary>
+		/// Creates a new endian-aware binary writer
+		/// </summary>
+		/// <param name="output">Output stream to write to</param>
+		/// <param name="encoding">Text encoding</param>
+		/// <param name="e">Endian to use</param>
+		public EndianBinaryWriter (Stream output, System.Text.Encoding encoding, Endian e = Endian.Big)
+			: base(output, encoding)
+		{
+			_endian = e;
+		}
+
+		/// <summary>
+		/// Writes a double-precision floating point number and advances the stream by 8 bytes
+		/// </summary>
+		public override void Write (double value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(double));
+			}
+		}
+
+		/// <summary>
+		/// Writes a signed short integer and advances the stream by 2 bytes
+		/// </summary>
+		public override void Write (short value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(short));
+			}
+		}
+
+		/// <summary>
+		/// Writes a signed integer and advances the stream by 4 bytes
+		/// </summary>
+		public override void Write (int value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(int));
+			}
+		}
+
+		/// <summary>
+		/// Writes a signed long integer and advances the stream by 8 bytes
+		/// </summary>
+		public override void Write (long value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(long));
+			}
+		}
+
+		/// <summary>
+		/// Writes a single-precision floating point number and advances the stream by 4 bytes
+		/// </summary>
+		public override void Write (float value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(float));
+			}
+		}
+
+		/// <summary>
+		/// Writes an unsigned short integer and advances the stream by 2 bytes
+		/// </summary>
+		public override void Write (ushort value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(ushort));
+			}
+		}
+
+		/// <summary>
+		/// Writes an unsigned integer and advances the stream by 4 bytes
+		/// </summary>
+		public override void Write (uint value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(uint));
+			}
+		}
+
+		/// <summary>
+		/// Writes an unsigned long integer and advances the stream by 8 bytes
+		/// </summary>
+		public override void Write (ulong value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				base.Write(_buffer, 0, sizeof(ulong));
+			}
+		}
+
+		/// <summary>
+		/// Writes a signed integer and advances the stream by 3 bytes
+		/// </summary>
+		public void WriteInt24 (int value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				int start = (Endian.Big == _endian) ? 1 : 0;
+				base.Write(_buffer, start, 3);
+			}
+		}
+
+		/// <summary>
+		/// Writes an unsigned integer and advances the stream by 3 bytes
+		/// </summary>
+		public void WriteUInt24 (uint value)
+		{
+			lock(_buffer)
+			{
+				_buffer.Insert(value, 0, _endian);
+				int start = (Endian.Big == _endian) ? 1 : 0;
+				base.Write(_buffer, start, 3);
+			}
+		}
+	}
+}
