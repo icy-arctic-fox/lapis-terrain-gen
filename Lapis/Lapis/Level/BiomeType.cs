bf8fe2844966903d862e0db537cfa8eef27b81d3
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-04 02:34:51 +0000

    Added BiomeType and GameMode Added data classes and some utility methods Uncommented some code that is ok now since new types have been imported

---
+++ Lapis/Lapis/Level/BiomeType.cs
@@ -0,0 +1,123 @@
+﻿namespace Lapis.Level
+{
+	/// <summary>
+	/// ID numbers for vanilla Minecraft biomes
+	/// </summary>
+	public enum BiomeType : byte
+	{
+		/// <summary>
+		/// Large open biome made entirely of water
+		/// </summary>
+		Ocean = 0,
+
+		/// <summary>
+		/// Relatively flat biome with rolling hills and tall grass
+		/// </summary>
+		Plains = 1,
+
+		/// <summary>
+		/// Consists mostly of sand and is very dry
+		/// </summary>
+		Desert = 2,
+
+		/// <summary>
+		/// Mountainous region
+		/// </summary>
+		ExtremeHills = 3,
+
+		/// <summary>
+		/// Lots of trees
+		/// </summary>
+		Forest = 4,
+
+		/// <summary>
+		/// Snowy forest
+		/// </summary>
+		Taiga = 5,
+
+		/// <summary>
+		/// Shallow pools of water with scattered land
+		/// </summary>
+		Swampland = 6,
+
+		/// <summary>
+		/// Curving water
+		/// </summary>
+		River = 7,
+
+		/// <summary>
+		/// Hell-like realm
+		/// </summary>
+		Nether = 8,
+
+		/// <summary>
+		/// Floating island in The End
+		/// </summary>
+		End = 9,
+
+		/// <summary>
+		/// Wide expansion of water that is covered in ice
+		/// </summary>
+		FrozenOcean = 10,
+
+		/// <summary>
+		/// A river that has been frozen over
+		/// </summary>
+		FrozenRiver = 11,
+
+		/// <summary>
+		/// Tundra - barren snowlands with no vegitation
+		/// </summary>
+		IcePlains = 12,
+
+		/// <summary>
+		/// Tundra region with mountains
+		/// </summary>
+		IceMountains = 13,
+
+		/// <summary>
+		/// An island covered with mushrooms and mycelium
+		/// </summary>
+		MushroomIsland = 14,
+
+		/// <summary>
+		/// The shoreline along a mushroom island
+		/// </summary>
+		MushroomIslandShore = 15,
+
+		/// <summary>
+		/// A sandy region between land and an ocean or river
+		/// </summary>
+		Beach = 16,
+
+		/// <summary>
+		/// Hills and dunes in the desert
+		/// </summary>
+		DesertHills = 17,
+
+		/// <summary>
+		/// Hills in a forest
+		/// </summary>
+		ForestHills = 18,
+
+		/// <summary>
+		/// Hills in a snowy forest
+		/// </summary>
+		TaigaHills = 19,
+
+		/// <summary>
+		/// Foothills to a mountainous region
+		/// </summary>
+		ExtremeHillsEdge= 20,
+
+		/// <summary>
+		/// Very dense and tropical biome
+		/// </summary>
+		Jungle = 21,
+
+		/// <summary>
+		/// Hills in the jungle
+		/// </summary>
+		JungleHills = 22
+	}
+}
---
+++ Lapis/Lapis/Level/GameMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Lapis.Level
+{
+	/// <summary>
+	/// Types of modes that the player may be in
+	/// </summary>
+	public enum GameMode : byte
+	{
+		/// <summary>
+		/// Players must survive, build, and craft on their own
+		/// </summary>
+		Survival = 0,
+
+		/// <summary>
+		/// Players can place and destroy blocks at will
+		/// </summary>
+		Creative = 1,
+
+		/// <summary>
+		/// Players cannot place or destroy blocks
+		/// </summary>
+		Adventure = 2
+	}
+}
