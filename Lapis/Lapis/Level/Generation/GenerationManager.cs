e12d20fe2296ff476e4640af59e539a4679eae80
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-09 23:28:53 +0000

    Changed how information about terrain generators are stored. Added methods for getting a terrain generator.

--- Lapis/Lapis/Level/Generation/GenerationManager.cs
+++ Lapis/Lapis/Level/Generation/GenerationManager.cs
@@ -14,8 +14,7 @@ namespace Lapis.Level.Generation
 	/// <remarks>Use this class to get available generators.</remarks>
 	public static class GenerationManager
 	{
-		private static readonly List<Type> _terrainGeneratorTypes = new List<Type>();
-		private static readonly List<string> _terrainGeneratorNames = new List<string>();
+		private static readonly Dictionary<string, List<Tuple<int, Type>>> _terrainGeneratorTypes = new Dictionary<string, List<Tuple<int, Type>>>();
 		private static readonly Type _terrainGeneratorType = typeof(ITerrainGenerator);
 
 		/// <summary>
@@ -26,12 +25,13 @@ namespace Lapis.Level.Generation
 			get
 			{
 				lock(_terrainGeneratorTypes)
-					return _terrainGeneratorNames.ToArray<string>();
+					return _terrainGeneratorTypes.Keys.ToArray();
 			}
 		}
 
 		#region Discovery
-		private const string GeneratorDir = "generators";
+		private const string GeneratorDir  = "generators";
+		private const string FileExtension = "*.dll";
 
 		/// <summary>
 		/// Runs on first reference to load available generators
@@ -56,9 +56,19 @@ namespace Lapis.Level.Generation
 															_terrainGeneratorType != t));
 
 			_terrainGeneratorTypes.Clear();
-			_terrainGeneratorTypes.AddRange(types);
-			_terrainGeneratorNames.AddRange(
-				types.Select(t => ((ITerrainGenerator)Activator.CreateInstance(t)).GeneratorName));
+			foreach(var type in types)
+			{
+				var instance = (ITerrainGenerator)Activator.CreateInstance(type);
+				var name     = instance.GeneratorName;
+				var version  = instance.GeneratorVersion;
+				List<Tuple<int, Type>> list;
+				if(_terrainGeneratorTypes.ContainsKey(name)) // Multiple versions under the same name
+					list = _terrainGeneratorTypes[name];
+				else // First seen
+					_terrainGeneratorTypes[name] = list = new List<Tuple<int, Type>>();
+				var item = new Tuple<int, Type>(version, type);
+				list.Add(item);
+			}
 		}
 
 		private static void loadGenerators (string path)
@@ -67,7 +77,7 @@ namespace Lapis.Level.Generation
 
 			if(Directory.Exists(path))
 			{
-				foreach(var file in Directory.EnumerateFiles(path, "*.dll", SearchOption.AllDirectories))
+				foreach(var file in Directory.EnumerateFiles(path, FileExtension, SearchOption.AllDirectories))
 				{
 					try
 					{
@@ -81,5 +91,58 @@ namespace Lapis.Level.Generation
 			}
 		}
 		#endregion
+
+		/// <summary>
+		/// Gets a terrain generator
+		/// </summary>
+		/// <param name="name">Name of the generator</param>
+		/// <returns>A terrain generator or null if the generator doesn't exist</returns>
+		/// <remarks>If there are multiple versions of the generator, the latest one is used.</remarks>
+		public static ITerrainGenerator GetGenerator (string name)
+		{
+			ITerrainGenerator generator = null;
+			lock(_terrainGeneratorTypes)
+			{
+				if(_terrainGeneratorTypes.ContainsKey(name))
+				{
+					var versions   = _terrainGeneratorTypes[name];
+					var maxVersion = versions[0].Item1;
+					var type       = versions[0].Item2;
+					for(var i = 1; i < versions.Count; ++i)
+					{// More than one version
+						var item = versions[i];
+						if(item.Item1 > maxVersion)
+						{// Found a newer version
+							maxVersion = item.Item1;
+							type       = item.Item2;
+						}
+					}
+					generator = (ITerrainGenerator)Activator.CreateInstance(type);
+				}
+			}
+			return generator;
+		}
+
+		/// <summary>
+		/// Gets a terrain generator
+		/// </summary>
+		/// <param name="name">Name of the generator</param>
+		/// <param name="version">Specific version of a generator</param>
+		/// <returns>A terrain generator or null if the generator (and version) doesn't exist</returns>
+		public static ITerrainGenerator GetGenerator (string name, int version)
+		{
+			ITerrainGenerator generator = null;
+			lock(_terrainGeneratorTypes)
+			{
+				if(_terrainGeneratorTypes.ContainsKey(name))
+				{
+					var versions = _terrainGeneratorTypes[name];
+					foreach(var item in versions)
+						if(version == item.Item1)
+							generator = (ITerrainGenerator)Activator.CreateInstance(item.Item2);
+				}
+			}
+			return generator;
+		}
 	}
 }
