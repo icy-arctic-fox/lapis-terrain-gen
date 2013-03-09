8dc61f309b34ba55d7c49ff0fe3f1bb3f28aa353
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-09 19:48:36 +0000

    Added an empty terrain generator

---
+++ Lapis/Terrain Generators/EmptyTerrainGenerator/EmptyTerrainGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Lapis.Level.Data;
+using Lapis.Level.Generation;
+
+namespace EmptyTerrainGenerator
+{
+	public class EmptyTerrainGenerator : ITerrainGenerator
+	{
+		public string GeneratorName
+		{
+			get { return "Empty Terrain Generator"; }
+		}
+
+		public int GeneratorVersion
+		{
+			get { return 1; }
+		}
+
+		public string GeneratorOptions
+		{
+			get { return String.Empty; }
+		}
+
+		public void Initialize (string options)
+		{
+			// ...
+		}
+
+		public ChunkData GenerateChunk (int cx, int cz)
+		{
+			return new ChunkData(cx, cz);
+		}
+	}
+}
