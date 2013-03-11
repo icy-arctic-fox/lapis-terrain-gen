bca3e28eecb25d192dc422da7369349448773057
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-11 17:15:11 +0000

    Added a class for waiting on a large amount of reset events (WaitList). Added the start of GenerationManager which will be used for generating chunks in bulk.

---
+++ Lapis/Lapis/Level/Generation/GenerationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lapis.Level.Generation
+{
+	/// <summary>
+	/// Manages generating chunks in bulk
+	/// </summary>
+	public class GenerationManager
+	{
+		/// <summary>
+		/// A unit of work
+		/// </summary>
+		private class GenerationUnit
+		{
+			
+		}
+	}
+}
