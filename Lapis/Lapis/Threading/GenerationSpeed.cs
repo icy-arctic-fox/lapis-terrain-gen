7960ab4112b359c54dd5e139935550ee2aeb4ba7
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-23 19:40:06 +0000

    The GenerateRectange() method now returns the number of blocks generated. Renamed GenerationManager to BulkGenerator.

---
+++ Lapis/Lapis/Threading/GenerationSpeed.cs
@@ -0,0 +1,18 @@
+﻿namespace Lapis.Threading
+{
+	/// <summary>
+	/// Speed at which to generate chunks
+	/// </summary>
+	public enum GenerationSpeed
+	{
+		Full,
+
+		Fast,
+
+		Medium,
+
+		Slow,
+
+		VerySlow
+	}
+}
