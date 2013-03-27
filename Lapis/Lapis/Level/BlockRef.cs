b49b3f51eafaf16bca52be5897692ca8cc0e0df9
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-27 22:13:37 +0000

    Added documentation. Added conversion (reference and de-reference) to reference types. Added comparators for reference types.

--- Lapis/Lapis/Level/BlockRef.cs
+++ Lapis/Lapis/Level/BlockRef.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lapis.Blocks;
 
 namespace Lapis.Level
 {
@@ -24,7 +25,7 @@ namespace Lapis.Level
 		/// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="bx"/> or <paramref name="bz"/> are outside the bounds of the chunk</exception>
 		internal BlockRef (ChunkRef chunk, byte bx, byte by, byte bz)
 		{
-			if(null == chunk)
+			if(ReferenceEquals(null, chunk))
 				throw new ArgumentNullException("chunk", "The chunk reference can't be null.");
 			if(Level.Chunk.Size <= bx)
 				throw new ArgumentOutOfRangeException("bx", "The block x-position must be inside the bounds of the chunk.");
@@ -38,67 +39,102 @@ namespace Lapis.Level
 		}
 
 		#region Properties
+		/// <summary>
+		/// X-position of the block within the chunk
+		/// </summary>
 		public byte BlockX
 		{
 			get { return _bx; }
 		}
 
+		/// <summary>
+		/// Y-position of the block within the chunk
+		/// </summary>
 		public byte BlockY
 		{
 			get { return _by; }
 		}
 
+		/// <summary>
+		/// Z-position of the block within the chunk
+		/// </summary>
 		public byte BlockZ
 		{
 			get { return _bz; }
 		}
 
+		/// <summary>
+		/// Global x-position of the block within the realm
+		/// </summary>
 		public int GlobalX
 		{
-			get { throw new NotImplementedException(); }
+			get { return _chunk.ChunkX * Level.Chunk.Size + _bx; }
 		}
 
+		/// <summary>
+		/// Global z-position of the block within the realm
+		/// </summary>
 		public int GlobalZ
 		{
-			get { throw new NotImplementedException(); }
+			get { return _chunk.ChunkZ * Level.Chunk.Size + _bz; }
 		}
 
+		/// <summary>
+		/// Chunk that the block is in
+		/// </summary>
 		public ChunkRef Chunk
 		{
 			get { return _chunk; }
 		}
 
+		/// <summary>
+		/// X-position of the chunk that contains the block within the realm
+		/// </summary>
 		public int ChunkX
 		{
 			get { return _chunk.ChunkX; }
 		}
 
+		/// <summary>
+		/// Z-position of the chunk that contains the block within the realm
+		/// </summary>
 		public int ChunkZ
 		{
 			get { return _chunk.ChunkZ; }
 		}
 
+		/// <summary>
+		/// Realm that the block is a part of
+		/// </summary>
 		public RealmRef Realm
 		{
 			get { return _chunk.Realm; }
 		}
 
+		/// <summary>
+		/// ID of the realm that the block is a part of
+		/// </summary>
 		public int RealmId
 		{
 			get { return _chunk.RealmId; }
 		}
 
+		/// <summary>
+		/// World that the block belongs to
+		/// </summary>
 		public WorldRef World
 		{
 			get { return _chunk.World; }
 		}
 
+		/// <summary>
+		/// Name of the world that the block belongs to
+		/// </summary>
 		public string WorldName
 		{
 			get { return _chunk.WorldName; }
 		}
 		#endregion
-
 
 		#region Neighbors
 		/// <summary>
@@ -191,5 +227,163 @@ namespace Lapis.Level
 			return null; // Y-position is invalid
 		}
 		#endregion
+
+		#region Conversion
+		/// <summary>
+		/// Forces a chunk to load (if it isn't loaded already) and returns a block object from within it
+		/// </summary>
+		/// <param name="block">Block reference to de-reference</param>
+		/// <returns>A loaded block or null if the block being referenced doesn't exist</returns>
+		/// <remarks>Null will be returned if <paramref name="block"/> is null.
+		/// The world, realm, and chunk that the block belongs to will also be loaded (if it hasn't been already).
+		/// If the block doesn't exist, it will be generated.</remarks>
+		public static implicit operator Block (BlockRef block)
+		{
+			if(!ReferenceEquals(null, block))
+			{
+				var chunk = (Chunk)block._chunk; // This cast forces the chunk to load
+				return (ReferenceEquals(null, chunk)) ? null : chunk.GetBlock(block._bx, block._by, block._bz);
+			}
+			return null;
+		}
+		#endregion
+
+		#region Comparison
+		/// <summary>
+		/// Checks if two block references are equal to each other
+		/// </summary>
+		/// <param name="refA">First block reference</param>
+		/// <param name="refB">Second block reference</param>
+		/// <returns>True if the block references appear to be equal</returns>
+		/// <remarks><paramref name="refA"/> and <paramref name="refB"/> are considered equal if they're both null or they're from the same world, realm, and chunk and have the same coordinate.</remarks>
+		public static bool operator == (BlockRef refA, BlockRef refB)
+		{
+			if(ReferenceEquals(refA, refB))
+				return true;
+			if(ReferenceEquals(null, refA))
+				return false;
+			if(ReferenceEquals(null, refB))
+				return false;
+			return refA._bx == refB._bx && refA._by == refB._by && refA._bz == refB._bz;
+		}
+
+		/// <summary>
+		/// Checks if two block references are not equal to each other
+		/// </summary>
+		/// <param name="refA">First block reference</param>
+		/// <param name="refB">Second block reference</param>
+		/// <returns>True if the block references don't appear to be equal</returns>
+		/// <remarks><paramref name="refA"/> and <paramref name="refB"/> are considered equal if they're both null or they're from the same world, realm, and chunk and have the same coordinate.</remarks>
+		public static bool operator != (BlockRef refA, BlockRef refB)
+		{
+			if(ReferenceEquals(refA, refB))
+				return false;
+			if(ReferenceEquals(null, refA))
+				return true;
+			if(ReferenceEquals(null, refB))
+				return true;
+			return refA._bx != refB._bx || refA._by != refB._by || refA._bz == refB._bz;
+		}
+
+		/// <summary>
+		/// Checks if a block reference and block are equal
+		/// </summary>
+		/// <param name="blockRef">Block reference</param>
+		/// <param name="block">Block</param>
+		/// <returns>True if the block reference and block appear to be equal</returns>
+		/// <remarks><paramref name="blockRef"/> and <paramref name="block"/> are considered equal if they're both null or contents from both blocks are the same.</remarks>
+		public static bool operator == (BlockRef blockRef, Block block)
+		{
+			if(ReferenceEquals(null, blockRef))
+				return ReferenceEquals(null, block);
+			if(ReferenceEquals(null, block))
+				return false;
+			var b = (Block)blockRef; // Force block to load
+			return block.Equals(b); // Compare contents
+		}
+
+		/// <summary>
+		/// Checks if a block reference and block are not equal
+		/// </summary>
+		/// <param name="blockRef">Block reference</param>
+		/// <param name="block">Block</param>
+		/// <returns>True if the block reference and block don't appear to be equal</returns>
+		/// <remarks><paramref name="blockRef"/> and <paramref name="block"/> are considered equal if they're both null or contents from both blocks are the same.</remarks>
+		public static bool operator != (BlockRef blockRef, Block block)
+		{
+			if(ReferenceEquals(null, blockRef))
+				return !ReferenceEquals(null, block);
+			if(ReferenceEquals(null, block))
+				return true;
+			var b = (Block)blockRef; // Force block to load
+			return !block.Equals(b); // Compare contents
+		}
+
+		/// <summary>
+		/// Checks if a block reference and block are equal
+		/// </summary>
+		/// <param name="block">Block</param>
+		/// <param name="blockRef">Block reference</param>
+		/// <returns>True if the block reference and block appear to be equal</returns>
+		/// <remarks><paramref name="blockRef"/> and <paramref name="block"/> are considered equal if they're both null or contents from both blocks are the same.</remarks>
+		public static bool operator == (Block block, BlockRef blockRef)
+		{
+			return blockRef == block;
+		}
+
+		/// <summary>
+		/// Checks if a block reference and block are not equal
+		/// </summary>
+		/// <param name="block">Block</param>
+		/// <param name="blockRef">Block reference</param>
+		/// <returns>True if the block reference and block don't appear to be equal</returns>
+		/// <remarks><paramref name="blockRef"/> and <paramref name="block"/> are considered equal if they're both null or contents from both blocks are the same.</remarks>
+		public static bool operator != (Block block, BlockRef blockRef)
+		{
+			return blockRef != block;
+		}
+
+		/// <summary>
+		/// Checks if the block reference is equal to another object
+		/// </summary>
+		/// <param name="obj">Object to check against</param>
+		/// <returns>True if <paramref name="obj"/> is considered equal to the block reference</returns>
+		/// <remarks><paramref name="obj"/> is considered equal if it is not null, is a BlockRef with the same world, realm, and coordinate, or a Block with the same world, realm, and coordinate.</remarks>
+		public override bool Equals (object obj)
+		{
+			var blockRef = obj as BlockRef;
+			if(!ReferenceEquals(null, blockRef))
+				return this == blockRef;
+			var block = obj as Block;
+			if(!ReferenceEquals(null, block))
+				return this == block;
+			return false;
+		}
+		#endregion
+
+		/// <summary>
+		/// Generages a hash code from the object
+		/// </summary>
+		/// <returns>A hash code</returns>
+		public override int GetHashCode ()
+		{
+			var hash = _chunk.GetHashCode();
+			hash *= 37;
+			hash ^= _bx;
+			hash *= 37;
+			hash ^= _by;
+			hash *= 37;
+			hash ^= _bz;
+			return hash;
+		}
+
+		/// <summary>
+		/// Gets the string representation of the world reference
+		/// </summary>
+		/// <returns>A string representing the world</returns>
+		public override string ToString ()
+		{
+			return _chunk + " Block: " + _bx + ", " + _by + ", " + _bz;
+		}
 	}
 }
--- Lapis/Lapis/Level/RealmRef.cs
+++ Lapis/Lapis/Level/RealmRef.cs
@@ -20,27 +20,198 @@ namespace Lapis.Level
 		/// <param name="id">ID of the realm</param>
 		internal RealmRef (WorldRef world, int id)
 		{
-			if(null == world)
+			if(ReferenceEquals(null, world))
 				throw new ArgumentNullException("world", "The world reference can't be null.");
 			_world = world;
 			_id    = id;
 		}
 
 		#region Properties
+		/// <summary>
+		/// ID of the realm
+		/// </summary>
 		public int RealmId
 		{
 			get { return _id; }
 		}
 
+		/// <summary>
+		/// World that the realm belongs to
+		/// </summary>
 		public WorldRef World
 		{
 			get { return _world; }
 		}
 
+		/// <summary>
+		/// Name of the world that the realm is a part of
+		/// </summary>
 		public string WorldName
 		{
 			get { return _world.WorldName; }
 		}
 		#endregion
+
+		#region Conversion
+		/// <summary>
+		/// Forces a realm to load (if it isn't loaded already) and returns the realm object
+		/// </summary>
+		/// <param name="realm">Realm reference to de-reference</param>
+		/// <returns>A loaded realm or null if the realm being referenced doesn't exist</returns>
+		/// <remarks>Null will be returned if <paramref name="realm"/> is null.
+		/// The world that the realm belongs to will also be loaded (if it hasn't been already).</remarks>
+		public static implicit operator Realm (RealmRef realm)
+		{
+			if(!ReferenceEquals(null, realm))
+			{
+				var world = (World)realm._world; // This cast forces the world to load
+				return (ReferenceEquals(null, world)) ? null : world.LoadRealm(realm._id);
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Creates a reference to a realm from a loaded realm
+		/// </summary>
+		/// <param name="realm">Realm to get a reference for</param>
+		/// <returns>A reference to the realm</returns>
+		/// <remarks>Null will be returned if <paramref name="realm"/> is null</remarks>
+		public static implicit operator RealmRef (Realm realm)
+		{
+			return (ReferenceEquals(null, realm)) ? null : new RealmRef(realm.World, realm.Id);
+		}
+		#endregion
+
+		#region Comparison
+		/// <summary>
+		/// Checks if two realm references are equal to each other
+		/// </summary>
+		/// <param name="refA">First realm reference</param>
+		/// <param name="refB">Second realm reference</param>
+		/// <returns>True if the realm references appear to be equal</returns>
+		/// <remarks><paramref name="refA"/> and <paramref name="refB"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator == (RealmRef refA, RealmRef refB)
+		{
+			if(ReferenceEquals(refA, refB))
+				return true;
+			if(ReferenceEquals(null, refA))
+				return false;
+			if(ReferenceEquals(null, refB))
+				return false;
+			return refA._id == refB._id;
+		}
+
+		/// <summary>
+		/// Checks if two realm references are not equal to each other
+		/// </summary>
+		/// <param name="refA">First realm reference</param>
+		/// <param name="refB">Second realm reference</param>
+		/// <returns>True if the realm references don't appear to be equal</returns>
+		/// <remarks><paramref name="refA"/> and <paramref name="refB"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator != (RealmRef refA, RealmRef refB)
+		{
+			if(ReferenceEquals(refA, refB))
+				return false;
+			if(ReferenceEquals(null, refA))
+				return true;
+			if(ReferenceEquals(null, refB))
+				return true;
+			return refA._id != refB._id;
+		}
+
+		/// <summary>
+		/// Checks if a realm reference and realm are equal
+		/// </summary>
+		/// <param name="realmRef">Realm reference</param>
+		/// <param name="realm">Realm</param>
+		/// <returns>True if the realm reference and realm appear to be equal</returns>
+		/// <remarks><paramref name="realmRef"/> and <paramref name="realm"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator == (RealmRef realmRef, Realm realm)
+		{
+			if(ReferenceEquals(null, realmRef))
+				return ReferenceEquals(null, realm);
+			if(ReferenceEquals(null, realm))
+				return false;
+			return realmRef._id == realm.Id;
+		}
+
+		/// <summary>
+		/// Checks if a realm reference and realm are not equal
+		/// </summary>
+		/// <param name="realmRef">Realm reference</param>
+		/// <param name="realm">Realm</param>
+		/// <returns>True if the realm reference and realm don't appear to be equal</returns>
+		/// <remarks><paramref name="realmRef"/> and <paramref name="realm"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator != (RealmRef realmRef, Realm realm)
+		{
+			if(ReferenceEquals(null, realmRef))
+				return !ReferenceEquals(null, realm);
+			if(ReferenceEquals(null, realm))
+				return true;
+			return realmRef._id != realm.Id;
+		}
+
+		/// <summary>
+		/// Checks if a realm reference and realm are equal
+		/// </summary>
+		/// <param name="realm">Realm</param>
+		/// <param name="realmRef">Realm reference</param>
+		/// <returns>True if the realm reference and realm appear to be equal</returns>
+		/// <remarks><paramref name="realmRef"/> and <paramref name="realm"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator == (Realm realm, RealmRef realmRef)
+		{
+			return realmRef == realm;
+		}
+
+		/// <summary>
+		/// Checks if a realm reference and realm are not equal
+		/// </summary>
+		/// <param name="realm">Realm</param>
+		/// <param name="realmRef">Realm reference</param>
+		/// <returns>True if the realm reference and realm don't appear to be equal</returns>
+		/// <remarks><paramref name="realmRef"/> and <paramref name="realm"/> are considered equal if they're both null or they're from the same world and have the same ID.</remarks>
+		public static bool operator != (Realm realm, RealmRef realmRef)
+		{
+			return realmRef != realm;
+		}
+
+		/// <summary>
+		/// Checks if the realm reference is equal to another object
+		/// </summary>
+		/// <param name="obj">Object to check against</param>
+		/// <returns>True if <paramref name="obj"/> is considered equal to the realm reference</returns>
+		/// <remarks><paramref name="obj"/> is considered equal if it is not null, is a RealmRef with the same world and ID, or a Realm with the same world and ID.</remarks>
+		public override bool Equals (object obj)
+		{
+			var realmRef = obj as RealmRef;
+			if(!ReferenceEquals(null, realmRef))
+				return this == realmRef;
+			var realm = obj as Realm;
+			if(!ReferenceEquals(null, realm))
+				return this == realm;
+			return false;
+		}
+		#endregion
+
+		/// <summary>
+		/// Generages a hash code from the object
+		/// </summary>
+		/// <returns>A hash code</returns>
+		public override int GetHashCode ()
+		{
+			var hash = _world.GetHashCode();
+			hash *= 37;
+			hash ^= _id.GetHashCode();
+			return hash;
+		}
+
+		/// <summary>
+		/// Gets the string representation of the world reference
+		/// </summary>
+		/// <returns>A string representing the world</returns>
+		public override string ToString ()
+		{
+			return _world + " Realm: " + _id;
+		}
 	}
 }
