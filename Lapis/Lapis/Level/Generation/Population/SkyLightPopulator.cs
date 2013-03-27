95b52e72ce8d71240fcee0bd2ffa630b0124a4fd
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-27 23:38:29 +0000

    Added simple processing to the sky light populator for semi-transparent blocks

--- Lapis/Lapis/Level/Generation/Population/SkyLightPopulator.cs
+++ Lapis/Lapis/Level/Generation/Population/SkyLightPopulator.cs
@@ -1,4 +1,6 @@
-﻿namespace Lapis.Level.Generation.Population
+﻿using System;
+
+namespace Lapis.Level.Generation.Population
 {
 	/// <summary>
 	/// Populates a chunk with sky light
@@ -52,11 +54,19 @@
 			for(var bx = (byte)0; bx < Chunk.Size; ++bx)
 				for (var bz = (byte)0; bz < Chunk.Size; ++bz)
 				{
+					// Quickly fill air blocks
 					var height = c.GetHighestBlockAt(bx, bz);
 					for(var by = (byte)(Chunk.Height - 1); by > height; --by)
 						c.SetSkyLight(bx, by, bz, Chunk.FullBrightness);
 
-					// TODO: Add reduction through semi-transparent blocks
+					// Reduce amount of sky light through semi-transparent blocks
+					var light = Chunk.FullBrightness;
+					for(var by = (byte)height; by > 0; --by)
+					{
+						var block = c.GetBlock(bx, by, bz);
+						light     = (byte)Math.Max(0, light - block.Opacity);
+						c.SetSkyLight(bx, by, bz, light);
+					}
 				}
 
 			// TODO: Add fading (for things like overhangs)
