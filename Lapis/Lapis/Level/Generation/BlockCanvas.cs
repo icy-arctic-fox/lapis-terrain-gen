306a3b5fc3d48417b1ae2afca47aacb7535f94c4
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-29 19:13:39 +0000

    Started work on a BlockCanvas class which will be useful for chunk populators. Added a XZYSection coordinate struct to reference chunk sections.

---
+++ Lapis/Lapis/Level/Generation/BlockCanvas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lapis.Spatial;
+
+namespace Lapis.Level.Generation
+{
+	/// <summary>
+	/// Provides an interface for creating structures made of blocks and then inserting them into chunks
+	/// </summary>
+	public class BlockCanvas : IDisposable
+	{
+
+		#region Disposable
+		/// <summary>
+		/// Disposes the contents of the object
+		/// </summary>
+		public void Dispose ()
+		{
+			Dispose(true);
+		}
+
+		/// <summary>
+		/// Cleans up resources held by the object
+		/// </summary>
+		/// <param name="disposing">True if child variables should be cleaned up</param>
+		protected virtual void Dispose (bool disposing)
+		{
+			// ...
+		}
+
+		~BlockCanvas ()
+		{
+			Dispose(false);
+		}
+		#endregion
+	}
+}
