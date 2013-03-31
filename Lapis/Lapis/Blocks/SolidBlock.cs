3e169b57c50cc8104f25a7c33036dce34413d7d0
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-31 18:32:50 +0000

    Added an abstract solid block

---
+++ Lapis/Lapis/Blocks/SolidBlock.cs
@@ -0,0 +1,27 @@
+﻿namespace Lapis.Blocks
+{
+	/// <summary>
+	/// A solid block that cannot be passed through
+	/// </summary>
+	public abstract class SolidBlock : Block
+	{
+		/// <summary>
+		/// Whether or not the block is solid (non-solid blocks can be passed through)
+		/// </summary>
+		/// <remarks>This value is true for all block types that inherit this class.</remarks>
+		public override bool IsSolid
+		{
+			get { return true; }
+		}
+
+		/// <summary>
+		/// Creates a new solid block
+		/// </summary>
+		/// <param name="data">Additional meta-data for the block</param>
+		protected SolidBlock (byte data)
+			: base(data)
+		{
+			// ...
+		}
+	}
+}
