b21986e562c638af9cbd4dfe8679e873f14fc842
Michael Miller <icy.arctic.fox@gmail.com>
2013-03-31 22:44:48 +0000

    Adding generated block types

---
+++ Lapis/Lapis/Blocks/BedBlock.cs
@@ -0,0 +1,85 @@
+namespace Lapis.Blocks
+{
+	public class BedBlock : Block
+	{
+		#region Properties
+		/// <summary>
+		/// Type that describes the block
+		/// </summary>
+		/// <remarks>By using this property, you can safely cast the block object.</remarks>
+		public override BlockType Type
+		{
+			get { return BlockType.Bed; }
+		}
+
+		/// <summary>
+		/// Whether or not the block is solid (non-solid blocks can be passed through)
+		/// </summary>
+		public override bool IsSolid
+		{
+			get { return true; }
+		}
+
+		/// <summary>
+		/// Whether or not the block obeys physics
+		/// </summary>
+		public override bool Physics
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Whether or not the block can catch fire
+		/// </summary>
+		public override bool Flammable
+		{
+			get { return true; }
+		}
+
+		/// <summary>
+		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
+		/// </summary>
+		public override int Opacity
+		{
+			get { return 0; }
+		}
+
+		/// <summary>
+		/// Amount of block light that the block gives off
+		/// </summary>
+		public override int Luminance
+		{
+			get { return 0; }
+		}
+
+		/// <summary>
+		/// Amount of resistance to blasts before being destroyed
+		/// </summary>
+		public override float BlastResistance
+		{
+			get { return 1f; }
+		}
+
+		// TODO: Implement NBT data for 'Music'
+		#endregion
+
+		/// <summary>
+		/// Creates a new bed block
+		/// </summary>
+		public BedBlock ()
+			: base(0)
+		{
+			// ...
+		}
+
+		/// <summary>
+		/// Creates a new bed block
+		/// </summary>
+		/// <param name="data">Additional meta-data for the block</param>
+		public BedBlock (byte data)
+			: base(data)
+		{
+			// ...
+		}
+	}
+}
---
+++ Lapis/Lapis/Blocks/WheatBlock.cs
@@ -0,0 +1,85 @@
+namespace Lapis.Blocks
+{
+	public class WheatBlock : Block
+	{
+		#region Properties
+		/// <summary>
+		/// Type that describes the block
+		/// </summary>
+		/// <remarks>By using this property, you can safely cast the block object.</remarks>
+		public override BlockType Type
+		{
+			get { return BlockType.Wheat; }
+		}
+
+		/// <summary>
+		/// Whether or not the block is solid (non-solid blocks can be passed through)
+		/// </summary>
+		public override bool IsSolid
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Whether or not the block obeys physics
+		/// </summary>
+		public override bool Physics
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Whether or not the block can catch fire
+		/// </summary>
+		public override bool Flammable
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
+		/// </summary>
+		public override int Opacity
+		{
+			get { return 0; }
+		}
+
+		/// <summary>
+		/// Amount of block light that the block gives off
+		/// </summary>
+		public override int Luminance
+		{
+			get { return 0; }
+		}
+
+		/// <summary>
+		/// Amount of resistance to blasts before being destroyed
+		/// </summary>
+		public override float BlastResistance
+		{
+			get { return 0f; }
+		}
+
+		// TODO: Implement meta-data values
+		#endregion
+
+		/// <summary>
+		/// Creates a new wheat block
+		/// </summary>
+		public WheatBlock ()
+			: base(0)
+		{
+			// ...
+		}
+
+		/// <summary>
+		/// Creates a new wheat block
+		/// </summary>
+		/// <param name="data">Additional meta-data for the block</param>
+		public WheatBlock (byte data)
+			: base(data)
+		{
+			// ...
+		}
+	}
+}
