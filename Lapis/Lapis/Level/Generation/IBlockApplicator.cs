a373529470cf03de475ffc92f353e9ea43d5b6cb
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-17 05:34:04 +0000

    Added an object parameter to the block applicator. This will prevent recalculating information that was already done by GetAffectedChunks.

--- Lapis/Lapis/Level/Generation/IBlockApplicator.cs
+++ Lapis/Lapis/Level/Generation/IBlockApplicator.cs
@@ -15,7 +15,9 @@ namespace Lapis.Level.Generation
 		/// <param name="yOff">Offset along the y-axis at the origin chunk's y = 0</param>
 		/// <param name="zOff">Offset along the z-axis at the origin chunk's z = 0</param>
 		/// <returns>A collection of chunks that are affected</returns>
-		IEnumerable<ChunkRef> GetAffectedChunks (ChunkRef origin, int xOff, int yOff, int zOff);
+		/// <remarks>The object part of key-value pair allows extra data to be stored and passed back in to ApplyToChunk.
+		/// This is ideal when ApplyToChunk would have to recalculate or reprocess values that were already done in this method.</remarks>
+		IEnumerable<KeyValuePair<ChunkRef, object>> GetAffectedChunks (ChunkRef origin, int xOff, int yOff, int zOff);
 
 		/// <summary>
 		/// Applies the region of blocks to a chunk
@@ -25,6 +27,7 @@ namespace Lapis.Level.Generation
 		/// <param name="xOff">Offset along the x-axis from the origin chunk's x = 0</param>
 		/// <param name="yOff">Offset along the y-axis from the origin chunk's y = 0</param>
 		/// <param name="zOff">Offset along the z-axis from the origin chunk's z = 0</param>
-		void ApplyToChunk (Chunk c, ChunkRef origin, int xOff, int yOff, int zOff);
+		/// <param name="extra">Extra data returned from GetAffectedChunks</param>
+		void ApplyToChunk (Chunk c, ChunkRef origin, int xOff, int yOff, int zOff, object extra);
 	}
 }
