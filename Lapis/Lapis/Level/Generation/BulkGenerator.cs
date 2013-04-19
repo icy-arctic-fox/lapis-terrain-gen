f798d206624fcafaecca57b5b2f40ea8d05ba273
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-19 04:07:19 +0000

    Moved BulkGenerator class to Generation package

--- Lapis/Lapis/Level/Generation/BulkGenerator.cs
+++ Lapis/Lapis/Level/Generation/BulkGenerator.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Threading;
-using Lapis.Level;
 using Lapis.Level.Generation.Population;
+using Lapis.Threading;
 
-namespace Lapis.Threading
+namespace Lapis.Level.Generation
 {
 	/// <summary>
 	/// Manages generating chunks in bulk
