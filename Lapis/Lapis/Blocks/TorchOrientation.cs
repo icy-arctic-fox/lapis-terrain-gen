d00c2e7fa9829a6e25ad71906cb63d11e1d79d09
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-21 14:50:15 +0000

    Added an enum for torch orientation/positioning. Added orientation properties and constructors to the torch blocks.

---
+++ Lapis/Lapis/Blocks/TorchOrientation.cs
@@ -0,0 +1,39 @@
+﻿namespace Lapis.Blocks
+{
+	/// <summary>
+	/// Positioning of a torch
+	/// </summary>
+	public enum TorchOrientation : byte
+	{
+		/// <summary>
+		/// Pointing east
+		/// </summary>
+		East = 0x1,
+
+		/// <summary>
+		/// Pointing west
+		/// </summary>
+		West = 0x2,
+
+		/// <summary>
+		/// Pointing south
+		/// </summary>
+		South = 0x3,
+
+		/// <summary>
+		/// Pointing north
+		/// </summary>
+		North = 0x4,
+
+		/// <summary>
+		/// On the floor (or on top of a block) pointing up
+		/// </summary>
+		Floor = 0x5,
+
+		/// <summary>
+		/// Unknown position
+		/// </summary>
+		/// <remarks>Documentation calls this value "in the ground", but that doesn't make sense for a torch.</remarks>
+		Unknown = 0x6
+	}
+}
