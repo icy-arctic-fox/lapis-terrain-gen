41d0fb3db7b5f0ef8b6bf9929bad35d869fb0eec
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-23 16:33:21 +0000

    Added a base crop block. The carrot, potato, and wheat block extend from the crop block. Added properties to the crop block for stages and fully grown.

--- Lapis/Lapis/Blocks/CarrotBlock.cs
+++ Lapis/Lapis/Blocks/CarrotBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class CarrotBlock : Block
+	public class CarrotBlock : CropBlock
 	{
 		#region Properties
 		/// <summary>
@@ -11,64 +11,6 @@ namespace Lapis.Blocks
 		{
 			get { return BlockType.Carrot; }
 		}
-
-		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block can catch fire
-		/// </summary>
-		public override bool Flammable
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of block light that the block gives off
-		/// </summary>
-		public override byte Luminance
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Amount of resistance to blasts before being destroyed
-		/// </summary>
-		public override float BlastResistance
-		{
-			get { return 0f; }
-		}
-
-		// TODO: Implement meta-data values
 		#endregion
 
 		/// <summary>
@@ -83,9 +25,9 @@ namespace Lapis.Blocks
 		/// <summary>
 		/// Creates a new carrot block
 		/// </summary>
-		/// <param name="data">Additional meta-data for the block</param>
-		public CarrotBlock (byte data)
-			: base(data)
+		/// <param name="stage">Age or stage that the crop is in (from 0 to 7)</param>
+		public CarrotBlock (byte stage)
+			: base(stage)
 		{
 			// ...
 		}
--- Lapis/Lapis/Blocks/WheatBlock.cs
+++ Lapis/Lapis/Blocks/WheatBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class WheatBlock : Block
+	public class WheatBlock : CropBlock
 	{
 		#region Properties
 		/// <summary>
@@ -11,64 +11,6 @@ namespace Lapis.Blocks
 		{
 			get { return BlockType.Wheat; }
 		}
-
-		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block can catch fire
-		/// </summary>
-		public override bool Flammable
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of block light that the block gives off
-		/// </summary>
-		public override byte Luminance
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Amount of resistance to blasts before being destroyed
-		/// </summary>
-		public override float BlastResistance
-		{
-			get { return 0f; }
-		}
-
-		// TODO: Implement meta-data values
 		#endregion
 
 		/// <summary>
@@ -83,9 +25,9 @@ namespace Lapis.Blocks
 		/// <summary>
 		/// Creates a new wheat block
 		/// </summary>
-		/// <param name="data">Additional meta-data for the block</param>
-		public WheatBlock (byte data)
-			: base(data)
+		/// <param name="stage">Age or stage that the crop is in (from 0 to 7)</param>
+		public WheatBlock (byte stage)
+			: base(stage)
 		{
 			// ...
 		}
