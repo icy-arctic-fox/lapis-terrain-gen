3e8feed8bc7a212eee44f281074668a55fc13b67
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-24 05:16:29 +0000

    Changed lit furnace block to extend from furnace block

--- Lapis/Lapis/Blocks/LitFurnaceBlock.cs
+++ Lapis/Lapis/Blocks/LitFurnaceBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class LitFurnaceBlock : Block
+	public class LitFurnaceBlock : FurnaceBlock
 	{
 		#region Properties
 		/// <summary>
@@ -13,46 +13,6 @@ namespace Lapis.Blocks
 		}
 
 		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return true; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block can catch fire
-		/// </summary>
-		public override bool Flammable
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 15; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
 		/// Amount of block light that the block gives off
 		/// </summary>
 		public override byte Luminance
@@ -60,38 +20,19 @@ namespace Lapis.Blocks
 			get { return 13; }
 		}
 
-		/// <summary>
-		/// Amount of resistance to blasts before being destroyed
-		/// </summary>
-		public override float BlastResistance
-		{
-			get { return 17.5f; }
-		}
-
-		// TODO: Implement meta-data values
-
 		// TODO: Implement NBT data for 'Furnace'
 		#endregion
 
 		/// <summary>
-		/// Direction that the furnace is facing
-		/// </summary>
-		public BlockOrientation Orientation
-		{
-			get { return (BlockOrientation)Data; }
-		}
-
-		/// <summary>
-		/// Creates a new lit furnace block
+		/// Creates a new (empty) lit furnace block
 		/// </summary>
 		public LitFurnaceBlock ()
-			: base(0)
 		{
 			// ...
 		}
 
 		/// <summary>
-		/// Creates a new lit furnace block
+		/// Creates a new (empty) lit furnace block
 		/// </summary>
 		/// <param name="data">Additional meta-data for the block</param>
 		public LitFurnaceBlock (byte data)
@@ -101,12 +42,12 @@ namespace Lapis.Blocks
 		}
 
 		/// <summary>
-		/// Creates a new lit furnace block
+		/// Creates a new (empty) lit furnace block
 		/// </summary>
 		/// <param name="orientation">Direction that the furnace is facing</param>
 		/// <remarks>The orientation of the furnace can't be Up or Down.</remarks>
 		public LitFurnaceBlock (BlockOrientation orientation)
-			: base((byte)orientation)
+			: base(orientation)
 		{
 			// ...
 		}
