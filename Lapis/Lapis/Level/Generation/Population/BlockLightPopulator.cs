2cc913af8a674a2cf250819d2a2b430f7d694581
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-24 21:57:50 +0000

    Adding a block light populator that does nothing (for now) - will implement later

---
+++ Lapis/Lapis/Level/Generation/Population/BlockLightPopulator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Lapis.Level.Generation.Population
+{
+	/// <summary>
+	/// Populates a chunk with block light
+	/// </summary>
+	public class BlockLightPopulator : IChunkPopulator
+	{
+		#region Meta-data
+		/// <summary>
+		/// Name of the chunk populator
+		/// </summary>
+		/// <remarks>The name of this populator is "Block Light Populator"</remarks>
+		public string Name
+		{
+			get { return "Block Light Populator"; }
+		}
+
+		/// <summary>
+		/// Version of the chunk populator
+		/// </summary>
+		/// <remarks>The version of this populator is 1</remarks>
+		public int Version
+		{
+			get { return 1; }
+		}
+
+		/// <summary>
+		/// Description of what the chunk populator does
+		/// </summary>
+		public string Description
+		{
+			get { return "Fills chunks with block light"; }
+		}
+
+		/// <summary>
+		/// Creator of the chunk populator
+		/// </summary>
+		/// <remarks>The creator of this populator is Mike Miller &lt;dotMaiku@gmail.com&gt;</remarks>
+		public string Author
+		{
+			get { return "Mike Miller <dotMaiku@gmail.com>"; }
+		}
+		#endregion
+
+		/// <summary>
+		/// Populates a chunk
+		/// </summary>
+		/// <param name="c">Chunk to populate</param>
+		public void PopulateChunk (Chunk c)
+		{
+			LightChunk(c);
+		}
+
+		/// <summary>
+		/// Lights a chunk with block light
+		/// </summary>
+		/// <param name="c">Chunk to light up</param>
+		/// <remarks>This method can be used to recalculate block light for a chunk.</remarks>
+		public static void LightChunk (Chunk c)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
