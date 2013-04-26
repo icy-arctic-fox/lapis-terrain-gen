24befd72d3162979099ebff799aa1763dc02a4f2
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-26 13:43:04 +0000

    Added a base plate block. Updated stone, wood, heavy, and light plate blocks to extend from it. Implemented data values for plate blocks.

--- Lapis/Lapis/Blocks/StonePlateBlock.cs
+++ Lapis/Lapis/Blocks/StonePlateBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class StonePlateBlock : Block
+	public class StonePlateBlock : PlateBlock
 	{
 		#region Properties
 		/// <summary>
@@ -13,51 +13,11 @@ namespace Lapis.Blocks
 		}
 
 		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
 		/// Whether or not the block can catch fire
 		/// </summary>
 		public override bool Flammable
 		{
 			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of block light that the block gives off
-		/// </summary>
-		public override byte Luminance
-		{
-			get { return 0; }
 		}
 
 		/// <summary>
@@ -67,15 +27,12 @@ namespace Lapis.Blocks
 		{
 			get { return 2.5f; }
 		}
-
-		// TODO: Implement meta-data values
 		#endregion
 
 		/// <summary>
 		/// Creates a new stone plate block
 		/// </summary>
 		public StonePlateBlock ()
-			: base(0)
 		{
 			// ...
 		}
@@ -86,6 +43,16 @@ namespace Lapis.Blocks
 		/// <param name="data">Additional meta-data for the block</param>
 		public StonePlateBlock (byte data)
 			: base(data)
+		{
+			// ...
+		}
+
+		/// <summary>
+		/// Creates a new stone plate block
+		/// </summary>
+		/// <param name="powered">Whether or not the plate is pressed and giving off power</param>
+		public StonePlateBlock (bool powered)
+			: base(powered)
 		{
 			// ...
 		}
