fbe3d394b17e5ef7debf5cb8eeaf7094f48122f7
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-26 01:44:33 +0000

    Added an interface for redstone source blocks

---
+++ Lapis/Lapis/Blocks/IRedstoneSourceBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lapis.Blocks
+{
+	public interface IRedstoneSourceBlock
+	{
+		/// <summary>
+		/// Whether or not the block is powered and giving off a redstone current
+		/// </summary>
+		bool Powered { get; }
+
+		/// <summary>
+		/// Strength of the redstone current that the block is giving off (0-15)
+		/// </summary>
+		byte CurrentStrength { get; }
+	}
+}
