3da91dc31dcf1c5c1ade25feada61c147fa61ae3
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-27 03:50:25 +0000

    Updated the melon and pumpkin stem blocks to extend from the crop block

--- Lapis/Lapis/Blocks/MelonStemBlock.cs
+++ Lapis/Lapis/Blocks/MelonStemBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class MelonStemBlock : Block
+	public class MelonStemBlock : CropBlock
 	{
 		#region Properties
 		/// <summary>
@@ -11,64 +11,6 @@ namespace Lapis.Blocks
 		{
 			get { return BlockType.MelonStem; }
 		}
-
-		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block can catch fire
-		/// </summary>
-		public override bool Flammable
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of block light that the block gives off
-		/// </summary>
-		public override byte Luminance
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Amount of resistance to blasts before being destroyed
-		/// </summary>
-		public override float BlastResistance
-		{
-			get { return 0f; }
-		}
-
-		// TODO: Implement meta-data values
 		#endregion
 
 		/// <summary>
@@ -83,9 +25,9 @@ namespace Lapis.Blocks
 		/// <summary>
 		/// Creates a new melon stem block
 		/// </summary>
-		/// <param name="data">Additional meta-data for the block</param>
-		public MelonStemBlock (byte data)
-			: base(data)
+		/// <param name="stage">Age or stage that the stem is in (from 0 to 7)</param>
+		public MelonStemBlock (byte stage)
+			: base(stage)
 		{
 			// ...
 		}
