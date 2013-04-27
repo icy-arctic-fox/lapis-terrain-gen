c514af71149085cc0aeabc5920206c7cb453f3ce
Michael Miller <icy.arctic.fox@gmail.com>
2013-04-27 02:17:09 +0000

    Changed the disabled redstone torch to extend from a redstone torch block

--- Lapis/Lapis/Blocks/DisabledRedstoneTorchBlock.cs
+++ Lapis/Lapis/Blocks/DisabledRedstoneTorchBlock.cs
@@ -1,6 +1,6 @@
 namespace Lapis.Blocks
 {
-	public class DisabledRedstoneTorchBlock : Block, IRedstoneSourceBlock
+	public class DisabledRedstoneTorchBlock : RedstoneTorchBlock
 	{
 		#region Properties
 		/// <summary>
@@ -13,68 +13,18 @@ namespace Lapis.Blocks
 		}
 
 		/// <summary>
-		/// Whether or not the block is solid (non-solid blocks can be passed through)
-		/// </summary>
-		public override bool IsSolid
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block obeys physics
-		/// </summary>
-		public override bool Physics
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Whether or not the block can catch fire
-		/// </summary>
-		public override bool Flammable
-		{
-			get { return false; }
-		}
-
-		/// <summary>
-		/// Amount of light the block absorbs (0 is fully transparent and 15 is fully opaque)
-		/// </summary>
-		public override byte Opacity
-		{
-			get { return 0; }
-		}
-
-		/// <summary>
-		/// Whether or not the block diffuses light
-		/// </summary>
-		public override bool Diffuse
-		{
-			get { return false; }
-		}
-
-		/// <summary>
 		/// Amount of block light that the block gives off
 		/// </summary>
 		public override byte Luminance
 		{
 			get { return 0; }
 		}
-
-		/// <summary>
-		/// Amount of resistance to blasts before being destroyed
-		/// </summary>
-		public override float BlastResistance
-		{
-			get { return 0f; }
-		}
-
-		// TODO: Implement meta-data values
 		#endregion
 
 		/// <summary>
 		/// Whether or not the torch is powered and giving off a redstone current
 		/// </summary>
-		public bool Powered
+		public override bool Powered
 		{
 			get { return false; }
 		}
@@ -82,24 +32,15 @@ namespace Lapis.Blocks
 		/// <summary>
 		/// Strength of the redstone current that the torch is giving off
 		/// </summary>
-		public byte CurrentStrength
+		public override byte CurrentStrength
 		{
 			get { return 0; }
-		}
-
-		/// <summary>
-		/// Positioning of the torch
-		/// </summary>
-		public TorchOrientation Orientation
-		{
-			get { return (TorchOrientation)Data; }
 		}
 
 		/// <summary>
 		/// Creates a new disabled redstone torch block
 		/// </summary>
 		public DisabledRedstoneTorchBlock ()
-			: base(0)
 		{
 			// ...
 		}
@@ -119,7 +60,7 @@ namespace Lapis.Blocks
 		/// </summary>
 		/// <param name="orientation">Position of the torch</param>
 		public DisabledRedstoneTorchBlock (TorchOrientation orientation)
-			: base((byte)orientation)
+			: base(orientation)
 		{
 			// ...
 		}
