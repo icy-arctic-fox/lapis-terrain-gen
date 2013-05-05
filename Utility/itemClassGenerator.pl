183f4d75b3318b74803744282c7d91b1e6e69f39
Michael Miller <icy.arctic.fox@gmail.com>
2013-05-05 05:09:26 +0000

    Adding the initial version of the item class generator script

---
+++ Utility/itemClassGenerator.pl
@@ -0,0 +1,193 @@
+#!/usr/bin/perl -w
+use strict;
+
+use constant OUTPUT_DIR => 'output/';
+
+my @items = qw(
+IronShovel
+IronPickaxe
+IronAxe
+FlintAndSteel
+Apple
+Bow
+Arrow
+Coal
+Diamond
+IronIngot
+GoldIngot
+IronSword
+WoodSword
+WoodShovel
+WoodPickaxe
+WoodAxe
+StoneSword
+StoneShovel
+StonePickaxe
+StoneAxe
+DiamondSword
+DiamondShovel
+DiamondPickaxe
+DiamondAxe
+Stick
+Bowl
+MushroomStew
+GoldSword
+GoldShovel
+GoldPickaxe
+GoldAxe
+Feather
+Gunpowder
+WoodHoe
+StoneHoe
+IronHoe
+DiamondHoe
+GoldHoe
+Seeds
+Wheat
+Bread
+LeatherCap
+LeatherTunic
+LeatherPants
+LeatherBoots
+ChainHelmet
+ChainChestplate
+ChainLeggings
+ChainBoots
+IronHelmet
+IronChestplate
+IronLeggings
+IronBoots
+DiamondHelmet
+DiamondChestplate
+DiamondLeggings
+DiamondBoots
+GoldHelmet
+GoldChestplate
+GoldLeggings
+GoldBoots
+Flint
+RawPorkchop
+CookedPorkchop
+Painting
+GoldApple
+Sign
+WoodDoor
+Bucket
+WaterBucket
+LavaBucket
+Minecart
+Saddle
+IronDoor
+Redstone
+Snowball
+Boat
+Leather
+Milk
+Brick
+Clay
+SugarCane
+Paper
+Book
+Slimeball
+ChestCart
+FurnaceCart
+Egg
+Compass
+FishingRod
+Clock
+GlowstoneDust
+RawFish
+CookedFish
+Dye
+Bone
+Sugar
+Cake
+Bed
+Diode
+Cookie
+Map
+Shears
+Melon
+PumpkinSeeds
+MelonSeeds
+RawBeef
+Steak
+RawChicken
+CookedChicked
+RottenFlesh
+EnderPearl
+BlazeRod
+GhastTear
+GoldNugget
+NetherWart
+Potion
+Bottle
+SpiderEye
+FermentedSpiderEye
+BlazePowder
+MagmaCream
+BrewingStand
+Cauldron
+EyeOfEnder
+GlisteringMelon
+SpawnEgg
+EnchantingBottle
+FireCharge
+BookAndQuill
+WrittenBook
+Emerald
+ItemFrame
+FlowerPot
+Carrot
+Potato
+BakedPotato
+PoisonousPotato
+EmptyMap
+GoldCarrot
+MobHead
+CarrotOnAStick
+NetherStar
+PumpkinPie
+FireworkRocket
+FireworkStar
+EnchantedBook
+Comparator
+NetherBrick
+NetherQuartz
+TntCart
+HopperCart
+Disc13
+CatDisc
+BlocksDisc
+ChirpDisc
+FarDisc
+MallDisc
+MellohiDisc
+StalDisc
+StradDisc
+WardDisc
+Disc11
+WaitDisc
+);
+
+foreach my $item (@items)	{
+	my $className = $item . 'Item';
+	my $fileName  = OUTPUT_DIR . $className . '.cs';
+	open(ITEM, '>', $fileName) or die "Failed to create $fileName - $!\n";
+	print<<END_ITEM_CLASS;
+namespace Lapis.Items
+{
+	public class $className : EnchantableItem
+	{
+		/// <summary>
+		/// Numerical ID of the item
+		/// </summary>
+		public short ItemId
+		{
+			get { return (short)ItemType.$item; }
+		}
+	}
+}
+END_ITEM_CLASS
+	close(ITEM);
+}
